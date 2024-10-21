597c8fb3b129bcdf7cb9163f46c3a03eb4e052ba
BruDosSant <brudossant@gmail.com>
2024-10-21 13:37:43 +0000

    Aloha

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a double: %zu byte(s)\n", sizeof(double));
 	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 
-    return 0;
+    return (0);
 }
