d93054ef747c66a387186e40ec86d2c778f15318
BruDosSant <brudossant@gmail.com>
2024-10-21 15:31:29 +0000

    Aloha

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n < 0)
+	{
 		printf("is negative\n");
+	}
 	if (n == 0)
+	{
 		printf("is zero\n");
+	}
 	if (n > 0)
-		printf("int""is positive\n");
+	{
+		printf("is positive\n");
+	}
 	return (0);
 }
