92f0e850ea4d2e7b593f6bdc29f073a8b7291f75
BruDosSant <brudossant@gmail.com>
2024-10-21 13:36:50 +0000

    Aloha

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-/**
- * main - Esta cosa me devuelve el peso que soporta cada arquitectura
- * Return: 0
- */
-int main(void)
+
+int main(void) 
 {
-	#if defined(__x86_64__)
-	printf("La arquitectura es de 64 bits.\n");
-	#elif defined(__i386__)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	#else
-	printf("Arquitectura desconocida.\n");
-	#endif
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a double: %zu byte(s)\n", sizeof(double));
+	printf("Size of a long double: %zu byte(s)\n", sizeof(long double));
 
-	return (0);
+    return 0;
 }
