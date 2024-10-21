77cf8893ee5e2ad0f7cc466569e9eecc20b7e309
BruDosSant <brudossant@gmail.com>
2024-10-21 19:22:50 +0000

    Aloha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - intento de putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char letra;
+
+	for (char letra = 'a'; letra <= 'z'; letra++)
+	{
+		putchar(letra);
+	}
+
+	return (0);
+}
