4769b42e206cdeee8c3b5eba986be6b890b194f5
BruDosSant <brudossant@gmail.com>
2024-10-21 13:29:27 +0000

    Aloha

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,10 @@ int main(void)
 	#if defined(__x86_64__)
 	printf("La arquitectura es de 64 bits.\n");
 	#elif defined(__i386__)
-	printf("La arquitectura es de 32 bits.\n");
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
 	#else
 	printf("Arquitectura desconocida.\n");
 	#endif
