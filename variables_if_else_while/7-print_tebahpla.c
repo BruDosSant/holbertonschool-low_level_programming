aa11fcb97714e4663f089fcfd9f1c09bc6a82492
BruDosSant <brudossant@gmail.com>
2024-10-21 21:40:29 +0000

    Aloha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - try try
+ * Return: 0
+ */
+int main(void)
+{
+	char letra;
+
+	for (letra = 'z'; letra >= 'a'; letra--)
+	{
+	putchar(letra);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
