af92f9d24e1ee8d0106aa7ed583a0008f3227271
BruDosSant <brudossant@gmail.com>
2024-10-21 22:02:17 +0000

    Aloha

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - orivle con los hexa
+ * Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	return (0);
+}
