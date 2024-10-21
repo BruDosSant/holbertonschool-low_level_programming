0d578b1e677449d9fc2adf41cb916d0bd3ecc71a
BruDosSant <brudossant@gmail.com>
2024-10-21 23:41:54 +0000

    Aloha

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,17 +12,17 @@ int main(void)
 	int max = 9;
 	int counters[length];
 
-		for (i = '0'; i < length; i++)
+		for (i = 0; i < length; i++)
 		{
 			counters[i] = 0;
 		}
 		for (;;)
 		{
-		for (i = length - '1'; i >= '0'; i--)
+		for (i = length - 1; i >= 0; i--)
 			printf("%d", counters[i]);
 			printf("\n");
 
-		for (i = '0'; i < length; i++)
+		for (i = 0; i < length; i++)
 		{
 		counters[i]++;
 
