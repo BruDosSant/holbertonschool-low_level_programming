d766f9994210f940287aec9dd1cf2b7e771995a2
BruDosSant <brudossant@gmail.com>
2024-10-22 19:33:53 +0000

    Aloha

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -13,13 +13,3 @@ void print_alphabet(void)
     }
     _putchar(10);
 }
-
-/**
- * main - punto de entrada del programa
- * Return: 0
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
