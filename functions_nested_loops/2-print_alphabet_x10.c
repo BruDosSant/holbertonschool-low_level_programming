a6c5603c3433fc29d85df4820be1dff7e4210537
BruDosSant <brudossant@gmail.com>
2024-10-22 21:02:05 +0000

    Aloha

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - ups i did it again
+ */
+
+void print_alphabet_x10(void)
+{
+	char az;
+	int cant;
+
+	for (cant = 0; cant <= 10; cant++)
+	{
+		for (az = 'a'; az <= 'z'; az++)
+		{
+			_putchar(az);
+		}
+		{
+			_putchar(10);
+		}
+	}
+}
