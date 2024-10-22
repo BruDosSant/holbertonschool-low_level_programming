c756c33d02f47204594f225318077a2585698727
BruDosSant <brudossant@gmail.com>
2024-10-22 23:38:30 +0000

    Aloha

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,9 +3,9 @@
 /**
  * _islower - Check if a character is lowercase
  * @c: The character to check
- * 
  * Return: 1 if the character is lowercase, 0 otherwise
  */
-int _islower(int c) {
-    return (c >= 'a' && c <= 'z') ? 1 : 0;
+int _islower(int c)
+{
+	return ((c >= 'a' && c <= 'z') ? 1 : 0);
 }
