73778b41282ec3895c0e83280fd6d68a862af56b
BruDosSant <brudossant@gmail.com>
2024-10-22 15:23:23 +0000

    Aloha

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,27 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - aloha
  * Return: 0
  */
 
-int main(void)
-{
-	char _putchar;
+int _putchar(char c) {
+    return (write(1, &c, 1));
+}
 
-		for (_putchar = 0; _putchar > 0;)
-	{
-		putchar(_putchar);
-	}
-	{
-		putchar('\n');
-	}
-	return (0);
+int main() {
+    _putchar('H');  // Imprime 'H'
+    _putchar('e');  // Imprime 'e'
+    _putchar('l');  // Imprime 'l'
+    _putchar('l');  // Imprime 'l'
+    _putchar('o');  // Imprime 'o'
+    _putchar(' ');  // Imprime un espacio
+    _putchar('W');  // Imprime 'W'
+    _putchar('o');  // Imprime 'o'
+    _putchar('r');  // Imprime 'r'
+    _putchar('l');  // Imprime 'l'
+    _putchar('d');  // Imprime 'd'
+    _putchar('!');  // Imprime '!'
+    _putchar('\n'); // Imprime una nueva línea
+    return 0;
 }
