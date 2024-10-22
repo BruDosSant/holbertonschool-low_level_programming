1d17795da0a61ee7026c41ba0ed64f720fafea20
BruDosSant <brudossant@gmail.com>
2024-10-22 15:27:22 +0000

    Aloha

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,19 +9,20 @@ int _putchar(char c) {
     return (write(1, &c, 1));
 }
 
-int main() {
-    _putchar('H');  // Imprime 'H'
-    _putchar('e');  // Imprime 'e'
-    _putchar('l');  // Imprime 'l'
-    _putchar('l');  // Imprime 'l'
-    _putchar('o');  // Imprime 'o'
-    _putchar(' ');  // Imprime un espacio
-    _putchar('W');  // Imprime 'W'
-    _putchar('o');  // Imprime 'o'
-    _putchar('r');  // Imprime 'r'
-    _putchar('l');  // Imprime 'l'
-    _putchar('d');  // Imprime 'd'
-    _putchar('!');  // Imprime '!'
-    _putchar('\n'); // Imprime una nueva línea
-    return 0;
+int main(void)
+{
+	_putchar('H');
+	_putchar('e');
+	_putchar('l');
+	_putchar('l');
+	_putchar('o');
+	_putchar(' ');
+	_putchar('W');
+	_putchar('o');
+	_putchar('r');
+	_putchar('l');
+	_putchar('d');
+	_putchar('!');
+	_putchar('\n');
+	return 0;
 }
