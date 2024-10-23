f4977a9b0564c78b2e2484e2597bbd5b101a3a9d
BruDosSant <brudossant@gmail.com>
2024-10-23 17:35:59 +0000

    Aloha

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * _abs - locura tricolor
+ * @int: numero al cual se le va a buscar su valor abs
+ * Return: valor absoluto del numero
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
