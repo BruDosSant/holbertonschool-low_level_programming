1599059c02b0c57879327ca577b7627986b9dce4
BruDosSant <brudossant@gmail.com>
2024-10-25 17:50:51 +0000

    Aloha

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,28 +1,23 @@
 #include "main.h"
-
 /**
  * jack_bauer - ups i did it again
  */
-
 void jack_bauer(void)
 {
 	char h1, h2, h3, m1, m2;
-	h1 = 0;
-	h2 = 0;
-	h3 = 0;
-	m1 = 0;
-	m2 = 0;
-	
-	_putchar(h1 + '0');
-	_putchar(h2 + '0');
-	_putchar(':');
-	_putchar(m1 + '0');
-	_putchar(m2 + '0');
-	_putchar('\n');
+	h1 = 0; h2 = 0; h3 = 0; m1 = 0; m2 = 0;
 
 	while (h3 < 3)
 	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+		
 		m2++;
+		
 		if (m2 > 9)
 		{
 			m1++;
@@ -38,15 +33,7 @@ void jack_bauer(void)
 			h1++;
 			h2 = 0;
 		}
-		if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
-		{
+		if (h1 == 2 && h2 > 3)
 			h3 = 3;
-		}
-		_putchar(h1 + '0');
-		_putchar(h2 + '0');
-		_putchar(':');
-		_putchar(m1 + '0');
-		_putchar(m2 + '0');
-		_putchar('\n');
 	}
 }
