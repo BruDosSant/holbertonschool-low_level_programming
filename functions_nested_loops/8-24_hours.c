9405a6fc130fe73d8d47b337d1c6576bb2a00928
BruDosSant <brudossant@gmail.com>
2024-10-25 17:29:40 +0000

    Aloha

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,20 +6,13 @@
 
 void jack_bauer(void)
 {
-	char h1, h2, m1, m2;
+	char h1, h2, h3, m1, m2;
 	h1 = 0;
 	h2 = 0;
 	m1 = 0;
 	m2 = 0;
 
-	_putchar(h1 + '0');
-	_putchar(h2 + '0');
-	_putchar(':');
-	_putchar(m1 + '0');
-	_putchar(m2 + '0');
-	_putchar('\n');
-
-	while (h1 < 3)
+	while (h3 < 3)
 	{
 		m2++;
 		if (m2 > 9)
@@ -37,9 +30,9 @@ void jack_bauer(void)
 			h1++;
 			h2 = 0;
 		}
-		if (h1 == 2 && h2 == 4)
+		if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
 		{
-			h1 = 3;
+			h3 = 3;
 		}
 		_putchar(h1 + '0');
 		_putchar(h2 + '0');
