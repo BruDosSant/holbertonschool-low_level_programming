7539c981679db2174b64ba4dce475ccc6df515c2
BruDosSant <brudossant@gmail.com>
2024-10-28 17:43:22 +0000

    Aloha

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - ups i did it again
+ */
+
+void more_numbers(void)
+{
+	int cant, num;
+
+	num = 0;
+	for (cant = 1; cant <= 10; cant++)
+	{
+		for (num = 0; num < 10; num++)
+		{
+			_putchar(num + '0');
+		}
+		for (num = 10; num <= 14; num++)
+		{
+			_putchar(num / 10 + '0');
+			_putchar(num % 10 + '0');
+		}
+		{
+			_putchar(10);
+		}
+	}
+}
