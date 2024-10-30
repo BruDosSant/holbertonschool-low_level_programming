822d25169a19c6d93d0c3c8e1d822932d160e6cf
BruDosSant <brudossant@gmail.com>
2024-10-30 15:33:26 +0000

    Aloha

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - imprime una linea n cantidad de veces
+ * @n: cant de veces que se imprimer n
+ */
+void print_diagonal(int n)
+{
+	int e;
+
+	int cant = 0;
+
+	if (cant == n)
+		_putchar(10);
+
+	while (cant < n)
+	{
+		e = 1;
+		while (e < cant)
+		{
+			_putchar(' ');
+			e++;
+		}
+		if (e == cant)
+		{
+			_putchar(47);
+			_putchar(10);
+		}
+		cant++;
+	}
+}
