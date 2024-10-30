1aea58ad70236f239088662d69b56b9fd2d9b921
BruDosSant <brudossant@gmail.com>
2024-10-30 18:36:00 +0000

    Aloha

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * print_diagonal - imprime una linea n cantidad de veces
+ * @n: cant de veces que se imprimer n
+ */
+void print_triangle(int size)
+{
+	int r, l, a;
+
+	l = 1;
+
+	if (size <= 0)
+		_putchar(10);
+	while (l <= size)
+	{
+		r = size - l;
+		while (r > 0)
+		{
+			_putchar(' ');
+			r--;
+		}
+		for (a = 1; a <= l; a++)
+		{
+			_putchar('#');
+		}
+		{
+			_putchar(10);
+		}
+		l++;
+	}
+}
