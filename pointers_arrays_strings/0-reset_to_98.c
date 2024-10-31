54ab65579e50058bdeaa040bf553e8c0569a83b8
BruDosSant <brudossant@gmail.com>
2024-10-31 15:50:02 +0000

    Aloha

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - lleva el valor de un int a 98
+ *@*n: es el pointer que lleva al int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Anananannanaa
+anashe
