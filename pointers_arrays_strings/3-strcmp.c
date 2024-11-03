404ee2922549b8afbc111e33784457788714571c
BruDosSant <brudossant@gmail.com>
2024-11-03 21:15:41 +0000

    Aloha

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * _strcmp - Funcion que compara dos char
+ * @s1: char 1
+ * @s2: char 2
+ * Return: positivo si s1 es mayor que s2 negativo si al revez y 0 si =
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] !='\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
