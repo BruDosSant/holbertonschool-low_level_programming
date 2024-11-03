c93f3e0d81ba452ce3b4f03351525d84c729cc85
BruDosSant <brudossant@gmail.com>
2024-11-03 23:15:23 +0000

    Aloha

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,23 @@
 
 char *cap_string(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-                str[i] -= 32;
-        }
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
 
-        while (str[i] != '\0')
-        {
-                if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == ';' || str[i] == ':')
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == ';' || str[i] == ':')
 		{
-                        if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-                        {
-                                str[i + 1] -= 32;
-                        }
-                }
-                i++;
-        }
-        return (str);
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+	return (str);
 }
