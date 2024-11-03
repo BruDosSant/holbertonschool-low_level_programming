e2640eb540335e02d76f68eb14aa56710ab01ef6
BruDosSant <brudossant@gmail.com>
2024-11-03 23:37:27 +0000

    Aloha

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,15 +12,15 @@ char *leet(char *p)
 
 	for (i = 0; p[i] != '\0'; i++)
 	{
-		if (p[i] == 'a' || p[i] == 'A')
+		while (p[i] == 'a' || p[i] == 'A')
 			p[i] = '4';
-		if (p[i] == 'e' || p[i] == 'E')
+		while (p[i] == 'e' || p[i] == 'E')
 			p[i] = '3';
-		if (p[i] == 'o' || p[i] == 'O')
+		while (p[i] == 'o' || p[i] == 'O')
 			p[i] = '0';
-		if (p[i] == 't' || p[i] == 'T')
+		while (p[i] == 't' || p[i] == 'T')
 			p[i] = '7';
-		if (p[i] == 'l' || p[i] == 'L')
+		while (p[i] == 'l' || p[i] == 'L')
 			p[i] = '1';
 	}
 	return (p);
