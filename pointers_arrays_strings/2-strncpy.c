be2796b25c1bf9cedb22e23360123f325b7508b5
BruDosSant <brudossant@gmail.com>
2024-11-03 20:54:53 +0000

    Aloha

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,40 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies up to n characters from the string src to dest.
- * @dest: The destination string where content is to be copied.
- * @src: The source string from which to copy.
- * @n: The maximum number of characters to copy from src.
- * 
- * Return: A pointer to the destination string dest.
+ * _strncpy - Copy a string starting from index 0 of `dest`.
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
+ * Return: `dest` edited string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-    char *original_dest = dest; // Save the original pointer to dest
-    int i;
+	int i;
 
-    // Copy up to n characters from src to dest
-    for (i = 0; i < n; i++)
-    {
-        if (*src != '\0') // If there are still characters in src
-        {
-            *dest = *src; // Copy character from src to dest
-            src++;        // Move to the next character in src
-        }
-        else
-        {
-            *dest = '\0'; // If src is shorter, fill dest with null
-        }
-        dest++; // Move to the next position in dest
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-    // Ensure remaining positions in dest are filled with null if needed
-    while (i < n)
-    {
-        *dest = '\0'; // Fill remaining dest with null bytes
-        dest++;
-        i++;
-    }
+	for (; n > i; i++)
+		dest[i] = '\0';
 
-    return (original_dest); // Return the original pointer to dest
+	return (dest);
 }
