24b5c27c245abfd8270f876f221569c9c88a8515
BruDosSant <brudossant@gmail.com>
2024-11-03 19:54:04 +0000

    Aloha

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - no entendi
+ * @dest: string al cual se le agregan
+ * @src: string que se agrefa
+ * @n: no entendi 
+ * Return: un mejunje de todo
+ */
+char *_strncat(char *dest, const char *src, int n) 
+{
+	char *dest_og = dest;
+	
+	int i;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (dest_og);
+}
