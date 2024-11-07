f55dc50fd6b192ec994773d981cfc80c641ce5c5
BruDosSant <brudossant@gmail.com>
2024-11-07 01:11:53 +0000

    Aloha

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memset - llena un bloque de memoria con un contenido especifico
+ * @s: puntero al bloque de memoria que se quiere llenar
+ * @b: el valor que se quiere asignar
+ * @n: la cantidad de bytes que se van a llenar
+ * Return: devuelve un puntero al espacio de memoria que se queria llenar
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned char *ptr = (unsigned char *)s;
