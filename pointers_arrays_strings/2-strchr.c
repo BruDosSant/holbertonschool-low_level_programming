b43a4bae4eaeafaf07ccc0dbcf051366375b518a
BruDosSant <brudossant@gmail.com>
2024-11-08 00:07:53 +0000

    Aloha

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - aloha
+ * @s: manin
+ * @c: alhoa
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+	{
+		s++;
+	}
+
+	return (s);
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+}
