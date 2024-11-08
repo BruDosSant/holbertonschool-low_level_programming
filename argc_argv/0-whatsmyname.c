9851357ceae4b09df114fc50a1dc4a8deedc2788
BruDosSant <brudossant@gmail.com>
2024-11-08 23:38:46 +0000

    Aloha

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,6 +5,7 @@
  * main - prints its name, followed by a new line
  * @argc: segun yo no hace nada
  * @argv: es lo que devuelve el nombre del programa
+ * Return: el nombre del programa
  */
 
 int main(int argc, char *argv[])
