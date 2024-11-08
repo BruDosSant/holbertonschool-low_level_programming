e70ede2dea63899b2a348c272fc4321920739cd0
BruDosSant <brudossant@gmail.com>
2024-11-08 23:51:21 +0000

    Aloha

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: ya wey
+ * @argv: parale por favor
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
+
