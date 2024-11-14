3c47d7803e0dc751e54724e77f8fbd075618971f
BruDosSant <brudossant@gmail.com>
2024-11-14 16:51:06 +0000

    Aloha

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,7 +8,10 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 	a = malloc(size * sizeof(char));
-
+	if(a == NULL)
+	{
+		return (NULL);	
+	}
 	if(size > 0)
 	{
 		for(i = 0; i <= size; i++)
