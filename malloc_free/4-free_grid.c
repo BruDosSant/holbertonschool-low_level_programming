a4af1b696391d0abb493a7cfc2481901ab4a5ee0
BruDosSant <brudossant@gmail.com>
2024-11-17 20:17:34 +0000

    Aloha

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function
+ * @grid: Puntero doble a lo que creo alloc grid
+ * @height: numero de renglones o lineas
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
