d8f470f4fb27420d0e8ee69e847402684cc4600a
BruDosSant <brudossant@gmail.com>
2024-11-17 23:57:20 +0000

    Aloha

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include<stddef.h>
+#include<stdio.h>
+
+/**
+ * print_dog - imprime detlles del dog
+ * @d: pointer a la data del dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(NULL);
+
+	if (d =! NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	printf("Age: %.6f\n", d->age);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
