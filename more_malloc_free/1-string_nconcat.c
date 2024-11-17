eaa263e8c8f4cba3998e98ef05e083e1b5212710
BruDosSant <brudossant@gmail.com>
2024-11-17 21:12:24 +0000

    Aloha

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -28,13 +28,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	result = malloc(len1 + n + 1);
 	if (result == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
+		result[i + j] = s2[j];
 
 	result[i + j] = '\0';
 
