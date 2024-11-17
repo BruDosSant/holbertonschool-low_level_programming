019604feba525e55e728ed1e7b072808da9fbb46
BruDosSant <brudossant@gmail.com>
2024-11-17 23:27:21 +0000

    Aloha

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include<stddef.h>
 
+/**
+ * init_dog - prints a struct dog
+ * @d: pointer a la estructura
+ * @name: nombre
+ * @age: edad
+ * @owner: propietario
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
