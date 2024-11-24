a1db673b8d6b60077ebcfcc0be62af1790e8ac8e
BruDosSant <brudossant@gmail.com>
2024-11-24 17:36:30 +0000

    Aloha

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
+/**
+ * print_name - toma un valor name y lo utilizza en una funcion f
+ * @name: valor que introduce en la funcion f (string)
+ * @f: nombre de la funcion que imprime name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
