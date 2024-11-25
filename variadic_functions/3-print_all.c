89a49fc198bc91cee5cc23090b35cc0fadc404a3
BruDosSant <brudossant@gmail.com>
2024-11-25 01:52:16 +0000

    Aloha

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,12 +2,6 @@
 #include<stdio.h>
 #include<stdarg.h>
 
-typedef struct printer
-{
-	char *symbol;
-	void (*print)(va_list);
-} printer_t;
-
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
