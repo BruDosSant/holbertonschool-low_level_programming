18dc2a687eeda5c1b078e7d96244454bf5a0c6d7
BruDosSant <brudossant@gmail.com>
2024-12-08 22:30:49 +0000

    Aloha

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -26,8 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (!new_node->str)
 	{
-		return (NULL);
 		free(new_node);
+		return (NULL);
 	}
 	new_node->len = len;
 	new_node->next = *head;
