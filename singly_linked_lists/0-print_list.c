3c997a7d5d9b3bd5264ee01534fc6cdb1b8c3ef8
BruDosSant <brudossant@gmail.com>
2024-12-08 21:15:08 +0000

    Aloha

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,27 @@
-#include "main.h"
+#include "lists.h"
 
+/**
+ * print_list - imprime el contenido de una lista mientras que este no se NULL
+ * @h: poointer a primer nodulo de la lista
+ * Return: cantidad de nodulos en la lista
+ */
 
+size_t print_list(const list_t *h)
+{
+	size_t count_nod = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		count_nod++;
+		h = h->next;
+	}
+	return (count_nod);
+}
