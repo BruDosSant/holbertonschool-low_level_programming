f45211ec52378f5da68b3bb8c36327827a0032e2
BruDosSant <brudossant@gmail.com>
2024-12-08 23:16:34 +0000

    Aloha

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * free_list - libera el espacio de todos los nodulos de una lista
+ * @head: puntero al comienzo de la lista (la vamos a recorrer)
+ */
+
 void free_list(list_t *head)
 {
 	list_t *temp;
