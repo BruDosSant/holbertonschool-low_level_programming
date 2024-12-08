08af198ab0446bb07d438ee15749d048827dd757
BruDosSant <brudossant@gmail.com>
2024-12-08 21:19:22 +0000

    Aloha

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,10 +4,18 @@
 #include<stdarg.h>
 #include<stdio.h>
 
-typedef struct list_s {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+/**
+ * struct list_s - structura de un nodulo?
+ * @str: string dentro del noduuloo
+ * @len: largo del string
+ * @next: pointer hacia el siguiente nodulo
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
