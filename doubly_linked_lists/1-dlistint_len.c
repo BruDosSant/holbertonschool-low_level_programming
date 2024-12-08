165328f5064030885e67edef34f39c29f541d6bb
BruDosSant <brudossant@gmail.com>
2024-12-08 23:51:14 +0000

    Aloha

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include<stddef.h>
+
+/**
+ * dlistint_len - devuelve el número de elementos e la list
+ * @h: puntero al primer nodulo de la lista
+ * Return: el número de elementos en la lista.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return count;
+}
