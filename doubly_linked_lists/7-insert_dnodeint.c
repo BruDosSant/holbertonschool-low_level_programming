6bcc6e18ecaa233eddb8d2e405093367f250c08b
BruDosSant <brudossant@gmail.com>
2024-12-09 18:48:31 +0000

    Aloha

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->next = temp->next;
 	new_node->prev = temp;
+	temp->next = new_node;
 	if (temp->next != NULL)
 		temp->next->prev = new_node;
 
